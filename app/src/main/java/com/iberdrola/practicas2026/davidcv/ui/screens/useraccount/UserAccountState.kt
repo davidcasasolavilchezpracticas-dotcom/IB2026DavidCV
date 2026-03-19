8909eb4fd609045b96b3f24ade2a4b13d886f10c
6003837 <david.casasola.vilchez.practicas@gmail.com>
2026-03-19 13:41:41 +0100

    refactor: Uso de state en UserAccount

--- app/src/main/java/com/iberdrola/practicas2026/davidcv/ui/screens/useraccount/UserAccountState.kt
+++
@@ -1,9 +0,0 @@
-package com.iberdrola.practicas2026.davidcv.ui.screens.useraccount
-
-import com.iberdrola.practicas2026.davidcv.domain.model.account.Account
-
-data class UserAccountState(
-    val account: Account? = null,
-    val isLoading: Boolean = false,
-    val error: String? = null
-)
