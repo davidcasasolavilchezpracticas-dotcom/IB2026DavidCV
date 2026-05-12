cb065576b601080e8e773e590d7c9d109dd54484
David <david.casasola.vilchez.practicas@gmail.com>
2026-05-12 09:13:54 +0200

    style: Añadido skeleton del botón de filtrado en la pantalla de facturas

---
+++ app/src/main/java/com/iberdrola/practicas2026/davidcv/ui/base/composables/billlist_empty/BillFilterButtonSkeleton.kt
@@ -0,0 +1,63 @@
+package com.iberdrola.practicas2026.davidcv.ui.base.composables.billlist_empty
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.iberdrola.practicas2026.davidcv.ui.base.composables.modifier_extensions.shimmerEffect
+import com.iberdrola.practicas2026.davidcv.ui.theme.SkeletonGreen
+
+@Composable
+@Preview
+fun BillFilterButtonSkeleton(
+    modifier: Modifier = Modifier
+) {
+    ConstraintLayout(
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        val startGuide = createGuidelineFromStart(0f)
+        val endGuide = createGuidelineFromEnd(0f)
+        val bottomGuide = createGuidelineFromBottom(0f)
+
+        val (historical, filterButton) = createRefs()
+
+        Box(
+            modifier = modifier
+                .height(24.dp)
+                .width(150.dp)
+                .constrainAs(historical){
+                    start.linkTo(startGuide)
+                    bottom.linkTo(bottomGuide)
+                }
+                .clip(RoundedCornerShape(4.dp))
+                .background(color = SkeletonGreen)
+                .shimmerEffect()
+        )
+
+        Box(
+            modifier = modifier
+                .height(32.dp)
+                .width(125.dp)
+                .constrainAs(filterButton) {
+                    end.linkTo(endGuide)
+                    bottom.linkTo(bottomGuide)
+                }
+                .fillMaxWidth(0.5f)
+                .clip(RoundedCornerShape(24.dp))
+                .background( color = SkeletonGreen )
+                .shimmerEffect()
+        )
+    }
+}
--- app/src/main/java/com/iberdrola/practicas2026/davidcv/ui/screens/billlist/BillListContentEmpty.kt
+++ app/src/main/java/com/iberdrola/practicas2026/davidcv/ui/screens/billlist/BillListContentEmpty.kt
@@ -1,18 +1,30 @@
 package com.iberdrola.practicas2026.davidcv.ui.screens.billlist
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.iberdrola.practicas2026.davidcv.ui.base.common.LocalSpacing
+import com.iberdrola.practicas2026.davidcv.ui.base.composables.billlist_empty.BillFilterButtonSkeleton
 import com.iberdrola.practicas2026.davidcv.ui.base.composables.billlist_empty.FacturaItemSkeleton
 import com.iberdrola.practicas2026.davidcv.ui.base.composables.billlist_empty.LastInvoiceSkeleton
+import com.iberdrola.practicas2026.davidcv.ui.base.composables.modifier_extensions.shimmerEffect
+import com.iberdrola.practicas2026.davidcv.ui.theme.SkeletonGreen
 
 /**
  * BillListContentEmpty
@@ -34,7 +46,11 @@ fun BillListContentEmpty(
     ) {
         LastInvoiceSkeleton(modifier)
 
-        Spacer(modifier = modifier.height(20.dp))
+        Spacer(modifier = modifier.height(4.dp))
+
+        BillFilterButtonSkeleton(modifier)
+
+        Spacer(modifier = modifier.height(4.dp))
 
         repeat(size) {
             FacturaItemSkeleton(modifier)
